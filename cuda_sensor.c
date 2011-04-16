922d5f7b0428cb60ba58de44459be297c68fca79
Mike Frisch <mfrisch@rogers.com>
2011-04-15 23:05:45 -0400

    Added return to main()

--- cuda_sensor.c
+++ cuda_sensor.c
@@ -15,6 +15,7 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <driver_types.h>
@@ -120,6 +121,8 @@ int main(int argc, char** argv)
             fflush(stdout);
 			}
 		}
+
+        return EXIT_SUCCESS;
 	}
 
 
